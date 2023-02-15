6e1f1648110d560e19ef45ca3ed3639fc855311b
Tim Kmecl <tim.kmecl.tk@gmail.com>
2023-02-15 10:26:06 +0100

    Part 6b exercise without toolkit

--- part6/redux-anecdotes/src/components/AnecdoteList.js
+++ part6/redux-anecdotes/src/components/AnecdoteList.js
@@ -2,7 +2,10 @@ import { useSelector, useDispatch } from 'react-redux'
 import { voteFor } from '../reducers/anecdoteReducer'
 
 const AnecdoteList = () => {
-	const anecdotes = useSelector(state => state.sort((a, b) => b.votes - a.votes))
+	const anecdotes = useSelector(state => {
+		const filteredAnecdotes = state.anecdotes.filter(anecdote => anecdote.content.toLowerCase().includes(state.filter.toLowerCase()))
+		return filteredAnecdotes.sort((a, b) => b.votes - a.votes)
+	})
   const dispatch = useDispatch()
 
   const vote = (id) => {
